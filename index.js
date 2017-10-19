d74ad1542c050a505abae966db468293b80ddcd4
Jirka Kremser <jkremser@redhat.com>
2017-10-19 10:52:16 +0200

    basic skeleton

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 'use strict';
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
+// const optionDefinitions = require('./lib/options');
+const usage = require('./lib/usage');
+// const exec = require('child_process').exec;
+// const execSync = require('child_process').execSync;
+// const spawn = require('child_process').spawn;
+// const path = require('path');
+
+usage.printUsage();
+
+try {
+  // spawn("docker", ["exec", "-it", "459147ca0a6c", "/bin/bash"], {stdio : "inherit"});
+} catch (e) {
+  console.log(e);
+}
+
 module.exports = (number, locale) => number.toLocaleString(locale);
